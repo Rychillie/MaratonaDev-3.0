7273dc6ec55a64e0f9e9786b798aeab41bb1cb68
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-02-19 23:58:35 -0300

    backend work

--- server.js
+++ server.js
@@ -1,16 +1,60 @@
-const cor = "branco"
-const tamanho = 2.5
+// configurar o servidor
+const express = require("express")
+const server = express()
 
-function verificarSeOCopoEstaSujo(sujo) {
-    //logica para verificar se o copo esta sujo
-    return `o copo: ${sujo}`
-}
+// configurar o servidor para apresentar arquivos estaticos
+server.use(express.static('public'))
 
-const copo = {
-    cor,
-    tamanho,
-    verificarSeOCopoEstaSujo,
-}
+// habilitar body do formulario
+server.use(express.urlencoded({ extended: true }))
 
-console.log(copo.verificarSeOCopoEstaSujo())
+// configurando a template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./",  {
+    express: server,
+    noCache: true, //boolean ou booleano só aceita 2 valores, verdadeiro ou falso
+})
 
+// lista de doadores: Vetor ou Array
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: "Marcos Vinicius",
+        blood: "A+"
+    },
+    {
+        name: "Matheus Teles",
+        blood: "O+"
+    },
+    {
+        name: "Luciano Santos",
+        blood: "B+"
+    },
+]
+
+// configurar a apresentação da página
+server.get("/", function(req, res) {
+    return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res) {
+    // pegar dados do formulario
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    return res.redirect("/")
+})
+
+// ligar o servidor e permitir o acesso na porta 3000
+server.listen(3000, function() {
+    console.log("Servidor rodando")
+})
