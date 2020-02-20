e20f65eb0ede2828b0f56e790928c3b84b4389ac
Rychillie <rychillie@hotmail.com>
2020-02-20 11:01:53 -0300

    final

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const server = express()
 // configurar o servidor para apresentar arquivos estaticos
 server.use(express.static('public'))
 
+// configurar a conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'admin',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+});
+
 // habilitar body do formulario
 server.use(express.urlencoded({ extended: true }))
 
@@ -15,29 +25,14 @@ nunjucks.configure("./",  {
     noCache: true, //boolean ou booleano só aceita 2 valores, verdadeiro ou falso
 })
 
-// lista de doadores: Vetor ou Array
-const donors = [
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Marcos Vinicius",
-        blood: "A+"
-    },
-    {
-        name: "Matheus Teles",
-        blood: "O+"
-    },
-    {
-        name: "Luciano Santos",
-        blood: "B+"
-    },
-]
-
 // configurar a apresentação da página
 server.get("/", function(req, res) {
-    return res.render("index.html", { donors })
+    db.query("SELECT * FROM donors", function(err, result) {
+        if (err) return res.send("erro no banco de dados.")
+
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
 })
 
 server.post("/", function(req, res) {
@@ -46,12 +41,27 @@ server.post("/", function(req, res) {
     const email = req.body.email
     const blood = req.body.blood
 
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    // se o nome for vazio
+    // se o email for vazio
+    // se o sangue for vazio
+    if (name == "" | email == "" | blood == "") {
+        return res.send("Todos os campos são obrigatorios.")
+    }
 
-    return res.redirect("/")
+    // coloca valores dentro do banco de dados
+    const query = `
+        INSERT INTO donors ("name", "email", "blood")
+        VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err) {
+        // fluxo de erro
+        if (err) return res.send("erro no banco de dados.")
+
+        // fluxo ideal
+        return res.redirect("/")
+    })
 })
 
 // ligar o servidor e permitir o acesso na porta 3000
